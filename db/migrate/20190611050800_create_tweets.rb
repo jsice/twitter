0f0f5571c7d26f24dfc7b1274b60afd847b09d54
wiwadh <jsice@Ice-YW.local>
2019-06-11 15:09:27 +0700

    Fix typo

--- db/migrate/20190611050800_create_tweets.rb
+++ db/migrate/20190611050800_create_tweets.rb
@@ -1,7 +1,7 @@
 class CreateTweets < ActiveRecord::Migration[6.0]
   def change
     create_table :tweets do |t|
-      t.string content
+      t.string 'content'
       
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,7 @@ ActiveRecord::Schema.define(version: 2019_06_11_050800) do
   enable_extension "plpgsql"
 
   create_table "tweets", force: :cascade do |t|
+    t.string "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
