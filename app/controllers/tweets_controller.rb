255046a7f213f3a072fc2d094e730e08f2639a29
wiwadh <jsice@Ice-YW.local>
2019-06-11 14:54:38 +0700

    Implement tweets' views and controller

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,2 +1,28 @@
 class TweetsController < ApplicationController
+
+  def index
+    @tweets = Tweet.all
+  end
+
+  def create
+    @tweet = Tweet.new(tweet_params)
+    redirect_to tweets_path
+  end
+
+  def show
+    @tweet = Tweet.find(params[:id])
+  end
+  
+  def destroy
+    @tweet = Tweet.find(params[:id])
+    @tweet.destroy
+    redirect_to tweets_path
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:content)
+  end
+
 end
