a16768c0d26013a5c0f744b860fac20f956d6365
wiwadh <jsice@Ice-YW.local>
2019-06-12 16:04:37 +0700

    Refector tweets' view and model

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -3,4 +3,7 @@ class Tweet < ApplicationRecord
   
   belongs_to :parent, class_name: "Tweet", foreign_key: "reply_id", optional: true
   has_many :replies, class_name: "Tweet", foreign_key: "reply_id"
+
+  scope :replies, -> { where.not(reply_id: nil) }
+  scope :tweets, -> { where(reply_id: nil) }
 end
