341b0b832bffb98352a812049f46d426552f8b8f
wiwadh <jsice@Ice-YW.local>
2019-06-27 16:31:37 +0700

    Add uniqueness validation in like, user_follower, users_retweet model

--- app/models/user_follower.rb
+++ app/models/user_follower.rb
@@ -1,4 +1,6 @@
 class UserFollower < ApplicationRecord
   belongs_to :follower, class_name: "User", foreign_key: "follower_id"
   belongs_to :following, class_name: "User", foreign_key: "following_id"
+
+  validates :following_id, uniqueness: { scope: :follower_id }
 end
