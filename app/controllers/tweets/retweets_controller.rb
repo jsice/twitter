99134af3c571f78d32d1f50f6aae4a8ed6620c0e
wiwadh <jsice@Ice-YW.local>
2019-06-27 15:39:19 +0700

    Create private method find_* (model) to reduce duplicated codes

--- app/controllers/tweets/retweets_controller.rb
+++ app/controllers/tweets/retweets_controller.rb
@@ -1,15 +1,20 @@
 class Tweets::RetweetsController < ApplicationController
 
   def create
-    tweet = Tweet.find(params[:tweet_id])
+    tweet = find_tweet
     UsersRetweet.create(tweet: tweet, user: current_user)
     redirect_back fallback_location: ''
   end
 
   def destroy
-    tweet = Tweet.find(params[:tweet_id])
+    tweet = find_tweet
     UsersRetweet.find_by(tweet: tweet, user: current_user)&.destroy
     redirect_back fallback_location: ''
+  end
+
+  private
+  def find_tweet
+    Tweet.find(params[:tweet_id]) or raise("not found")
   end
 
 end
--- app/controllers/users/followers_controller.rb
+++ app/controllers/users/followers_controller.rb
@@ -1,14 +1,19 @@
 class Users::FollowersController < ApplicationController
   def create
-    user = User.find(params[:user_id])
+    user = find_user
     UserFollower.create(following: user, follower: current_user) if user != current_user
     redirect_back fallback_location: ''
   end
 
   def destroy
-    user = User.find(params[:user_id])
+    user = find_user
     UserFollower.find_by(following: user, follower: current_user)&.destroy
     redirect_back fallback_location: ''
+  end
+  
+  private
+  def find_user
+    User.find(params[:user_id]) or raise("not found")
   end
   
 end
