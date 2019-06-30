aec73f35d0ea0a3b6e961a90897974cd19d2c2e7
wiwadh <jsice@Ice-YW.local>
2019-07-01 04:56:51 +0700

    Add test case on validations context

--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Tweet, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validations' do
+    let(:user) { User.create name: "Name", email: "test@test.com", password: "password" }
+    subject { described_class.new user: user, content: "Content" }
+
+    it 'is valid with a user and content' do
+      expect(subject).to be_valid
+    end
+
+    it 'is not valid without a user' do
+      subject.user = nil
+      expect(subject).to be_invalid
+    end
+
+    it 'is not valid without a content' do
+      subject.content = nil
+      expect(subject).to be_invalid
+    end
+
+    it 'is not valid with a content longer than 256' do
+      subject.content = "a" * 257
+      expect(subject).to be_invalid
+    end
+
+    it 'is not valid with blank content' do
+      subject.content = ""
+      expect(subject).to be_invalid
+    end
+
+  end
 end
