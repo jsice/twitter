2142f3607704423008f3f86aa5f747c25af7aa72
wiwadh <jsice@Ice-YW.local>
2019-07-01 09:06:33 +0700

    Add test cases in users validations context

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validations' do
+    subject { User.new name: "Name", email: "test@test.com", password: "password" }
+
+    it 'is valid with name, email and password' do
+      expect(subject).to be_valid
+    end
+
+    it 'is not valid without an email' do
+      subject.email = nil
+      expect(subject).to be_invalid
+    end
+
+    it 'is not valid without a password' do
+      subject.password = nil
+      expect(subject).to be_invalid
+    end
+
+    it 'is not valid without a name' do
+      subject.name = nil
+      expect(subject).to be_invalid
+    end
+
+    it 'is not valid with a name longer than 20' do
+      subject.name = "a" * 21
+      expect(subject).to be_invalid
+    end
+
+    it 'is not valid with blank name' do
+      subject.name = ""
+      expect(subject).to be_invalid
+    end
+
+  end
 end
