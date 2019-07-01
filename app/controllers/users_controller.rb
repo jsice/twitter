76fb8083b56e8ab33dbf187cb7d9d3b16eaed718
wiwadh <jsice@Ice-YW.local>
2019-07-01 14:17:22 +0700

    Add content tab to user page (Tweets, Followers, Followings);

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,10 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @tweets = @user.tweets.order('published_at DESC').paginate(page: params[:page], per_page: 5)
+    @followers = @user.followers.paginate(page: params[:page], per_page: 15)
+    @followings = @user.followings.paginate(page: params[:page], per_page: 15)
+    @tweets_size = @user.tweets.size
+    @followers_size = @user.followers.size
+    @followings_size = @user.followings.size
   end
 end
